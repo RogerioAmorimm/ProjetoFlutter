2a72dbf593351e38ecee169e675ca68d2fe7f13a
RogerioAmorim <rogerio.amorim98@gmail.com>
2021-10-23 11:36:58 -0300

    Ajuste App Bar

--- lib/components/AppBar/app_bar_componente.dart
+++ lib/components/AppBar/app_bar_componente.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:projetoflutter/components/drawerNavegacao/controller/drawer_navegacao_controller.dart';
 import 'package:projetoflutter/components/fade_in.dart';
+import 'package:projetoflutter/core/login/controller/login_controller.dart';
+import 'package:projetoflutter/core/login/page/login_page.dart';
+import 'package:projetoflutter/core/usuario/controller/usuario_controller.dart';
+import 'package:projetoflutter/ioc/service_locator.dart';
+import 'package:projetoflutter/modulos/listaItens/store/card_store.dart';
 import 'package:projetoflutter/utils/constants.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AppBarComponente {
-  static Future<bool> goToLogin(BuildContext context) async => true;
+  static Future<bool> goToLogin(BuildContext context) async {
+    await locator<UsuarioController>().getUsuarioLogado()!.limparToken();
+    locator<DrawerNavegacaoController>().limpar();
+    locator<CardStore>().limpar();
+    await Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(
+        builder: (BuildContext context) =>
+            LoginPage(controller: LoginController()),
+      ),
+      (Route route) => false,
+    );
+    return true;
+  }
+
   AppBar buildAppBar(
       GlobalKey<ScaffoldState> key,
       ThemeData tema,
       BuildContext context,
       dynamic loadingController,
       dynamic headerAniInterval) {
-    final Widget botaoVoltar = IconButton(
-        color: Constants.primaria,
+    final controller = locator<DrawerNavegacaoController>();
+
+    final Widget botaoMenu = IconButton(
+        color: Constants.primariaContraste,
         icon: Icon(FontAwesomeIcons.bars),
         onPressed: () => key.currentState!.openDrawer());
 
     final botaoSair = IconButton(
-        color: Constants.primaria,
+        color: Constants.primariaContraste,
         icon: Icon(FontAwesomeIcons.signOutAlt),
         onPressed: () => goToLogin(context));
 
@@ -28,9 +49,9 @@ class AppBarComponente {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 20),
             child: Hero(
-              tag: "TAG AQUI",
+              tag: 'near.huscarl.loginsample.logo',
               child: Image.asset(
-                "IMG AQUI",
+                Constants.logo,
                 filterQuality: FilterQuality.high,
                 height: 30,
               ),
@@ -42,7 +63,7 @@ class AppBarComponente {
 
     return AppBar(
       leading: FadeIn(
-        child: botaoVoltar,
+        child: controller.selectedindex == 0 ? Container() : botaoMenu,
         offset: .3,
         curve: headerAniInterval,
         fadeDirection: FadeDirection.startToEnd,
